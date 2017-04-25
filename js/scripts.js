d2363f7a8b20c729ef4139637c1d5c5686b5ef34
Jared Luedtke and Roger Tobias <jared+roger@>
2017-04-25 14:08:44 -0700

    oops, added content, basic style, and partial script functions

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+  $("#btn-js").click(function() {
+    $("#btn-js").hide();
+    $("#return-js").show();
+    $("#return-js").click(function() {
+      $("#return-js").hide();
+      $("#btn-js").show();
+    });
+  });
+
+  $("#btn-ops").click(function() {
+    $("#btn-ops").hide();
+    $("#return-ops").show();
+    $("#return-ops").click(function() {
+      $("#return-ops").hide();
+      $("#btn-ops").show();
+    });
+  });
+
+  $("#btn-var").click(function() {
+    $("#btn-var").hide();
+    $("#return-var").show();
+    $("#return-var").click(function() {
+      $("#return-var").hide();
+      $("#btn-var").show();
+    });
+  });
+
+  $("#btn-var-name-conventions").click(function() {
+    $("#btn-var-name-conventions").hide();
+    $("#return-var-name-conventions").show();
+    $("#return-var-name-conventions").click(function() {
+      $("#return-var-name-conventions").hide();
+      $("#btn-var-name-conventions").show();
+    });
+  });
+
+  $("#btn-functions").click(function() {
+    $("#btn-functions").hide();
+    $("#return-functions").show();
+    $("#return-functions").click(function() {
+      $("#return-functions").hide();
+      $("#btn-functions").show();
+    });
+  });
+
+  $("#btn-meth").click(function() {
+    $("#btn-meth").hide();
+    $("#return-meth").show();
+    $("#return-meth").click(function() {
+      $("#return-meth").hide();
+      $("#btn-meth").show();
+    });
+  });
+
+  $("#btn-ops").click(function() {
+    $("#btn-ops").hide();
+    $("#return-ops").show();
+    $("#return-ops").click(function() {
+      $("#return-ops").hide();
+      $("#btn-ops").show();
+    });
+  });
+
+});
