196fbf151e96872be7c7b374d849edd743bff586
Jared Luedtke and Roger Tobias <jared+roger@>
2017-04-25 14:23:54 -0700

    finished getting all of the buttons working with exception to the special button

--- js/scripts.js
+++ js/scripts.js
@@ -54,13 +54,125 @@ $(document).ready(function() {
     });
   });
 
-  $("#btn-ops").click(function() {
-    $("#btn-ops").hide();
-    $("#return-ops").show();
-    $("#return-ops").click(function() {
-      $("#return-ops").hide();
-      $("#btn-ops").show();
+  $("#btn-argue").click(function() {
+    $("#btn-argue").hide();
+    $("#return-argue").show();
+    $("#return-argue").click(function() {
+      $("#return-argue").hide();
+      $("#btn-argue").show();
     });
   });
+
+  $("#btn-para").click(function() {
+    $("#btn-para").hide();
+    $("#return-para").show();
+    $("#return-para").click(function() {
+      $("#return-para").hide();
+      $("#btn-para").show();
+    });
+  });
+
+  $("#btn-return").click(function() {
+    $("#btn-return").hide();
+    $("#return-return").show();
+    $("#return-return").click(function() {
+      $("#return-return").hide();
+      $("#btn-return").show();
+    });
+  });
+
+  $("#btn-chaining-meth").click(function() {
+    $("#btn-chaining-meth").hide();
+    $("#return-chaining-meth").show();
+    $("#return-chaining-meth").click(function() {
+      $("#return-chaining-meth").hide();
+      $("#btn-chaining-meth").show();
+    });
+  });
+
+  $("#btn-strings").click(function() {
+    $("#btn-strings").hide();
+    $("#return-strings").show();
+    $("#return-strings").click(function() {
+      $("#return-strings").hide();
+      $("#btn-strings").show();
+    });
+  });
+
+  $("#btn-bool").click(function() {
+    $("#btn-bool").hide();
+    $("#return-bool").show();
+    $("#return-bool").click(function() {
+      $("#return-bool").hide();
+      $("#btn-bool").show();
+    });
+  });
+
+  $("#btn-undef").click(function() {
+    $("#btn-undef").hide();
+    $("#return-undef").show();
+    $("#return-undef").click(function() {
+      $("#return-undef").hide();
+      $("#btn-undef").show();
+    });
+  });
+
+  $("#btn-nan").click(function() {
+    $("#btn-nan").hide();
+    $("#return-nan").show();
+    $("#return-nan").click(function() {
+      $("#return-nan").hide();
+      $("#btn-nan").show();
+    });
+  });
+
+  $("#btn-esc").click(function() {
+    $("#btn-esc").hide();
+    $("#return-esc").show();
+    $("#return-esc").click(function() {
+      $("#return-esc").hide();
+      $("#btn-esc").show();
+    });
+  });
+
+  $("#btn-alert").click(function() {
+    $("#btn-alert").hide();
+    $("#return-alert").show();
+    $("#return-alert").click(function() {
+      $("#return-alert").hide();
+      $("#btn-alert").show();
+    });
+  });
+
+  $("#btn-comms").click(function() {
+    $("#btn-comms").hide();
+    $("#return-comms").show();
+    $("#return-comms").click(function() {
+      $("#return-comms").hide();
+      $("#btn-comms").show();
+    });
+  });
+
+  $("#btn-jq").click(function() {
+    $("#btn-jq").hide();
+    $("#return-jq").show();
+    $("#return-jq").click(function() {
+      $("#return-jq").hide();
+      $("#btn-jq").show();
+    });
+  });
+
+  $("#btn-attr").click(function() {
+    $("#btn-attr").hide();
+    $("#return-attr").show();
+    $("#return-attr").click(function() {
+      $("#return-attr").hide();
+      $("#btn-attr").show();
+    });
+  });
+
+  $("btn-scare").click(function() {
+    
+  })
 
 });
